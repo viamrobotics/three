6711586e2dbeb30abef1064b20c2a73d6f5908f2
Micheal Parks <micheal.parks@outlook.com>
2023-01-19 14:53:17 -0500

    add npm publishing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,20 @@ export default defineConfig({
       // the proper extensions will be added
       fileName: 'ov',
     },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['three'],
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: undefined,
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          three: 'THREE',
+        },
+      },
+    }
   },
-  test: {}
+  test: {},
 })

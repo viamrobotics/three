5fefa68f9a2631b86f32ed7e3e896c092035444a
Micheal Parks <micheal.parks@outlook.com>
2024-02-29 12:52:08 -0500

    fix prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 module.exports = require('@viamrobotics/prettier-config');
-=======
-'use strict'
-
-module.exports = '@viamrobotics/prettier-config';
->>>>>>> 3aea62f3aab31018508a69ba3cc6087ced38086a
